2ed567267f5bdd9fd60e3dbac9eaed019e30d8a4
mjl371 <mjl371@m.scnu.edu.cn>
2017-06-21 23:07:27 +0800

    v3.1.2

--- StocktTradingSimulationSystem/main.py
+++ StocktTradingSimulationSystem/main.py
@@ -1,17 +1,11 @@
-import sys
-
-import WindowFuntion
-
-# import qdarkstyle
+import sys, WindowFuntion
 
 from PyQt5.QtWidgets import QApplication
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = WindowFuntion.MainWindow()
-    qss_file = open('qss//q.qss').read()
+    qss_file = open('qss//style.qss').read()
     mainWindow.setStyleSheet(qss_file)
-    # mainWindow.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     mainWindow.show()
     sys.exit(app.exec_())
